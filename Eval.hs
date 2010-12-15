4c97b7e4cc6fa93359631b38c1976125c7a862d6
bendmorris <ben@bendmorris.com>
2010-12-15 00:26:41 +0000

    Applied list splitting to strings.

--- Eval.hs
+++ Eval.hs
@@ -74,6 +74,7 @@ weval exp vars = case exp of
                                                      Name n -> pattern_match b d
                                                      Split x y -> case weval c vars of
                                                                     Result (List l) -> pattern_match b d
+                                                                    Result (Str l) -> pattern_match b d
                                                                     otherwise -> False
                                                      Pattern v -> if (weval c vars) == Result v 
                                                                   then pattern_match b d
@@ -82,9 +83,14 @@ weval exp vars = case exp of
                        funcall f (h:t) = case fst h of
                                             Name _ -> Def (fst h) (eager_eval (snd h)) (funcall f t)
                                             Split x y -> case eager_eval (snd h) of
-                                                            Val (List l) -> Def (Name x) (eager_eval (head l)) (
-                                                                            Def (Name y) (Val (List (tail l))) (
-                                                                            funcall f t))
+                                                            Val (List l) -> if length l > 0 then Def (Name x) (eager_eval (head l)) (
+                                                                                                 Def (Name y) (Val (List (tail l))) (
+                                                                                                 funcall f t))
+                                                                                            else Undefined "Can't split empty list"
+                                                            Val (Str l) -> if length l > 0 then Def (Name x) (Val (Str ([head l]))) (
+                                                                                                Def (Name y) (Val (Str (tail l))) (
+                                                                                                funcall f t))
+                                                                                           else Undefined "Can't split empty string"
                                             Pattern _ -> funcall f t
                        eager_eval x = case (weval (x) vars) of
                                         Result r -> Val r
