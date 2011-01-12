2b699505ab1462acf3bae4babfaf020a3ec6e6b1
bendmorris <ben@bendmorris.com>
2011-01-11 23:33:37 -0700

    Completing operator overloading support for all operators.

--- Calc.hs
+++ Calc.hs
@@ -24,93 +24,93 @@ type_mismatch f a b = Exception $ "Type mismatch: " ++
                                   
 -- calc: calls a function on the value of two Calculations, resulting in an exception if
 --       either Calculation previously resulted in an exception
-calc :: Expr -> Expr -> (Value -> Value -> Expr) -> Expr
+calc :: Expr -> Expr -> (Expr -> Expr -> Expr) -> Expr
 calc _ (Exception s) _ = Exception s
 calc (Exception s) _ _ = Exception s
-calc (Val a) (Val b) f = f a b
-calc a b f = Exception $ show a ++ " " ++ show b
+calc a b f = f a b
 
 -- the following functions provide basic operations between Values, returning an Expr
 -- addition
-vadd, vsub, vprod, vdiv, vexp, veq, vgt, vlt :: Value -> Value -> Expr
-vadd (NumInt a) (NumInt b) = Val (NumInt (a + b))
-vadd (NumFloat a) (NumFloat b) = Val (NumFloat (a + b))
-vadd (NumInt a) (NumFloat b) = Val (NumFloat ((realToFrac a) + b))
-vadd (NumFloat a) (NumInt b) = Val (NumFloat (a + (realToFrac b)))
-vadd (Str a) (Str b) = Val (Str (a ++ b))
-vadd (Str a) (NumInt b) = Val (Str (a ++ (show b)))
-vadd (NumInt a) (Str b) = Val (Str ((show a) ++ b))
-vadd (Str a) (NumFloat b) = Val (Str (a ++ (show b)))
-vadd (NumFloat a) (Str b) = Val (Str ((show a) ++ b))
-vadd (List a) (List b) = Val (List (a ++ b))
-vadd (Str a) (List []) = Val (Str a)
-vadd (List []) (Str b) = Val (Str b)
-vadd (List a) (v) = Val (List (a ++ [v]))
-vadd (v) (List b) = Val (List ([v] ++ b))
-vadd (Hash a) (Hash b) = Val (Hash [(b !! n) ++ (a !! n) | n <- [0..(hashSize - 1)]])
-vadd a b = Func (Name "add") [Val a, Val b]
+vadd, vsub, vprod, vdiv, vexp, veq, vgt, vlt :: Expr -> Expr -> Expr
+vadd (Val (NumInt a)) (Val (NumInt b)) = Val (NumInt (a + b))
+vadd (Val (NumFloat a)) (Val (NumFloat b)) = Val (NumFloat (a + b))
+vadd (Val (NumInt a)) (Val (NumFloat b)) = Val (NumFloat ((realToFrac a) + b))
+vadd (Val (NumFloat a)) (Val (NumInt b)) = Val (NumFloat (a + (realToFrac b)))
+vadd (Val (Str a)) (Val (Str b)) = Val (Str (a ++ b))
+vadd (Val (Str a)) (Val (NumInt b)) = Val (Str (a ++ (show b)))
+vadd (Val (NumInt a)) (Val (Str b)) = Val (Str ((show a) ++ b))
+vadd (Val (Str a)) (Val (NumFloat b)) = Val (Str (a ++ (show b)))
+vadd (Val (NumFloat a)) (Val (Str b)) = Val (Str ((show a) ++ b))
+vadd (Val (List a)) (Val (List b)) = Val (List (a ++ b))
+vadd (Val (Str a)) (Val (List [])) = Val (Str a)
+vadd (Val (List [])) (Val (Str b)) = Val (Str b)
+vadd (Val (List a)) (Val v) = Val (List (a ++ [v]))
+vadd (Val v) (Val (List b)) = Val (List ([v] ++ b))
+vadd (Val (Hash a)) (Val (Hash b)) = Val (Hash [(b !! n) ++ (a !! n) | n <- [0..(hashSize - 1)]])
+vadd a b = Func (Name "add") [a, b]
 -- subtraction
-vsub (NumInt a) (NumInt b) = Val (NumInt (a - b))
-vsub (NumFloat a) (NumFloat b) = Val (NumFloat (a - b))
-vsub (NumInt a) (NumFloat b) = Val (NumFloat ((realToFrac a) - b))
-vsub (NumFloat a) (NumInt b) = Val (NumFloat (a - (realToFrac b)))
-vsub a b = Func (Name "subtract") [Val a, Val b]
+vsub (Val (NumInt a)) (Val (NumInt b)) = Val (NumInt (a - b))
+vsub (Val (NumFloat a)) (Val (NumFloat b)) = Val (NumFloat (a - b))
+vsub (Val (NumInt a)) (Val (NumFloat b)) = Val (NumFloat ((realToFrac a) - b))
+vsub (Val (NumFloat a)) (Val (NumInt b)) = Val (NumFloat (a - (realToFrac b)))
+vsub a b = Func (Name "subtract") [a, b]
 -- multiplication
-vprod (NumInt a) (NumInt b) = Val (NumInt (a * b))
-vprod (NumFloat a) (NumFloat b) = Val (NumFloat (a * b))
-vprod (NumInt a) (NumFloat b) = Val (NumFloat ((realToFrac a) * b))
-vprod (NumFloat a) (NumInt b) = Val (NumFloat (a * (realToFrac b)))
-vprod (Str s) (NumInt b) = Val (Str (foldl (++) "" (take (fromIntegral b) (repeat s))))
-vprod (NumInt b) (Str s) = Val (Str (foldl (++) "" (take (fromIntegral b) (repeat s))))
-vprod (List l) (NumInt b) = Val (List (foldl (++) [] (take (fromIntegral b) (repeat l))))
-vprod (NumInt b) (List l) = Val (List (foldl (++) [] (take (fromIntegral b) (repeat l))))
-vprod a b = Func (Name "multiply") [Val a, Val b]
+vprod (Val (NumInt a)) (Val (NumInt b)) = Val (NumInt (a * b))
+vprod (Val (NumFloat a)) (Val (NumFloat b)) = Val (NumFloat (a * b))
+vprod (Val (NumInt a)) (Val (NumFloat b)) = Val (NumFloat ((realToFrac a) * b))
+vprod (Val (NumFloat a)) (Val (NumInt b)) = Val (NumFloat (a * (realToFrac b)))
+vprod (Val (Str s)) (Val (NumInt b)) = Val (Str (foldl (++) "" (take (fromIntegral b) (repeat s))))
+vprod (Val (NumInt b)) (Val (Str s)) = Val (Str (foldl (++) "" (take (fromIntegral b) (repeat s))))
+vprod (Val (List l)) (Val (NumInt b)) = Val (List (foldl (++) [] (take (fromIntegral b) (repeat l))))
+vprod (Val (NumInt b)) (Val (List l)) = Val (List (foldl (++) [] (take (fromIntegral b) (repeat l))))
+vprod a b = Func (Name "multiply") [a, b]
 -- division
 div_by_zero = Exception "Division by zero"
-vdiv (NumInt a) (NumInt 0) = div_by_zero
-vdiv (NumInt a) (NumFloat 0) = div_by_zero
-vdiv (NumFloat a) (NumInt 0) = div_by_zero
-vdiv (NumFloat a) (NumFloat 0) = div_by_zero
-vdiv (NumInt a) (NumInt b) = Val (NumInt (a `div` b))
-vdiv (NumFloat a) (NumFloat b) = Val (NumFloat (a / b))
-vdiv (NumInt a) (NumFloat b) = Val (NumFloat ((realToFrac a) / b))
-vdiv (NumFloat a) (NumInt b) = Val (NumFloat (a / (realToFrac b)))
-vdiv a b = Func (Name "divide") [Val a, Val b]
+vdiv (Val (NumInt a)) (Val (NumInt 0)) = div_by_zero
+vdiv (Val (NumInt a)) (Val (NumFloat 0)) = div_by_zero
+vdiv (Val (NumFloat a)) (Val (NumInt 0)) = div_by_zero
+vdiv (Val (NumFloat a)) (Val (NumFloat 0)) = div_by_zero
+vdiv (Val (NumInt a)) (Val (NumInt b)) = Val (NumInt (a `div` b))
+vdiv (Val (NumFloat a)) (Val (NumFloat b)) = Val (NumFloat (a / b))
+vdiv (Val (NumInt a)) (Val (NumFloat b)) = Val (NumFloat ((realToFrac a) / b))
+vdiv (Val (NumFloat a)) (Val (NumInt b)) = Val (NumFloat (a / (realToFrac b)))
+vdiv a b = Func (Name "divide") [a, b]
 -- exponent
-vexp (NumInt a) (NumInt b) = if b > 0 then Val (NumInt (a ^ b)) 
-                             else Val (NumFloat ((realToFrac a) ** (realToFrac b)))
-vexp (NumFloat a) (NumFloat b) = Val (NumFloat (a ** b))
-vexp (NumInt a) (NumFloat b) = Val (NumFloat ((realToFrac a) ** b))
-vexp (NumFloat a) (NumInt b) = Val (NumFloat (a ** (realToFrac b)))
-vexp a b = type_mismatch "^" a b
+vexp (Val (NumInt a)) (Val (NumInt b)) = if b > 0 then Val (NumInt (a ^ b)) 
+                                         else Val (NumFloat ((realToFrac a) ** (realToFrac b)))
+vexp (Val (NumFloat a)) (Val (NumFloat b)) = Val (NumFloat (a ** b))
+vexp (Val (NumInt a)) (Val (NumFloat b)) = Val (NumFloat ((realToFrac a) ** b))
+vexp (Val (NumFloat a)) (Val (NumInt b)) = Val (NumFloat (a ** (realToFrac b)))
+vexp a b = Func (Name "exp") [a, b]
 -- equality
-veq (NumInt a) (NumInt b) = Val (Bit (a == b))
-veq (NumFloat a) (NumFloat b) = Val (Bit (a == b))
-veq (NumInt a) (NumFloat b) = Val (Bit ((realToFrac a) == b))
-veq (NumFloat a) (NumInt b) = Val (Bit (a == (realToFrac b)))
-veq (List a) (List b) = Val (Bit (a == b))
-veq (Str a) (Str b) = Val (Bit (a == b))
-veq (Bit a) (Bit b) = Val (Bit (a == b))
-veq (Atom a b) (Atom c d) = Val (Bit (a == c && b == d))
-veq (Hash a) (Hash b) = Val (Bit (a == b))
-veq a b = Val (Bit False)
+veq (Val (NumInt a)) (Val (NumInt b)) = Val (Bit (a == b))
+veq (Val (NumFloat a)) (Val (NumFloat b)) = Val (Bit (a == b))
+veq (Val (NumInt a)) (Val (NumFloat b)) = Val (Bit ((realToFrac a) == b))
+veq (Val (NumFloat a)) (Val (NumInt b)) = Val (Bit (a == (realToFrac b)))
+veq (Val (Str a)) (Val (Str b)) = Val (Bit (a == b))
+veq (Val (List a)) (Val (List b)) = Val (Bit (a == b))
+veq (Val (Hash a)) (Val (Hash b)) = Val (Bit (a == b))
+veq (Val (HFunc a)) (Val (HFunc b)) = Val (Bit (a == b))
+veq a b = case a == b of
+            True -> Val (Bit True)
+            False -> Func (Name "eq") [a, b]
 -- greater than
-vgt (NumInt a) (NumInt b) = Val (Bit (a > b))
-vgt (NumFloat a) (NumFloat b) = Val (Bit (a > b))
-vgt (NumInt a) (NumFloat b) = Val (Bit ((realToFrac a) > b))
-vgt (NumFloat a) (NumInt b) = Val (Bit (a > (realToFrac b)))
-vgt a b = type_mismatch ">" a b
+vgt (Val (NumInt a)) (Val (NumInt b)) = Val (Bit (a > b))
+vgt (Val (NumFloat a)) (Val (NumFloat b)) = Val (Bit (a > b))
+vgt (Val (NumInt a)) (Val (NumFloat b)) = Val (Bit ((realToFrac a) > b))
+vgt (Val (NumFloat a)) (Val (NumInt b)) = Val (Bit (a > (realToFrac b)))
+vgt a b = Func (Name "gt") [a, b]
 -- less than
-vlt (NumInt a) (NumInt b) = Val (Bit (a < b))
-vlt (NumFloat a) (NumFloat b) = Val (Bit (a < b))
-vlt (NumInt a) (NumFloat b) = Val (Bit ((realToFrac a) < b))
-vlt (NumFloat a) (NumInt b) = Val (Bit (a < (realToFrac b)))
-vlt a b = type_mismatch "<" a b
+vlt (Val (NumInt a)) (Val (NumInt b)) = Val (Bit (a < b))
+vlt (Val (NumFloat a)) (Val (NumFloat b)) = Val (Bit (a < b))
+vlt (Val (NumInt a)) (Val (NumFloat b)) = Val (Bit ((realToFrac a) < b))
+vlt (Val (NumFloat a)) (Val (NumInt b)) = Val (Bit (a < (realToFrac b)))
+vlt a b = Func (Name "lt") [a, b]
 -- binary and
-vand (Bit a) (Bit b) = Val (Bit (a && b))
+vand (Val (Bit a)) (Val (Bit b)) = Val (Bit (a && b))
 vand a b = type_mismatch "&" a b
 -- binary or
-vor (Bit a) (Bit b) = Val (Bit (a || b))
+vor (Val (Bit a)) (Val (Bit b)) = Val (Bit (a || b))
 vor a b = type_mismatch "|" a b
 
 
